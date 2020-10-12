480dcfd4ea0acf4e998c790df16c0ee6e83409f0
Marcel Łukomski <marcel.lukomski@gmail.com>
2020-10-12 22:15:55 +0200

    Add socket functionality

--- client/app.js
+++ client/app.js
@@ -5,8 +5,10 @@ const addMessageForm = document.querySelector('#add-messages-form');
 const userNameInput = document.querySelector('#username');
 const messageContentInput = document.querySelector('#message-content');
 let userName;
+const socket = io();
 
-loginForm.addEventListener('submit', login);
+socket.on('message', ({ author, content }) => addMessage(author, content));
+;loginForm.addEventListener('submit', login);
 addMessageForm.addEventListener('submit', sendMessage);
 
 function login(event) {
@@ -17,6 +19,7 @@ function login(event) {
     userName = userNameInput.value;
     loginForm.classList.remove('show');
     messagesSection.classList.add('show');
+    socket.emit('join', userName);
   }
 };
 
@@ -26,6 +29,7 @@ function sendMessage(event) {
     alert('Please type your message!');
   } else {
     addMessage(userName, messageContentInput.value);
+    socket.emit('message', { author: userName, content: messageContentInput.value });
     messageContentInput.value = '';
   }
 };
@@ -34,10 +38,12 @@ function addMessage(author, content) {
   const message = document.createElement('li');
   message.classList.add('message');
   message.classList.add('message--received');
-  if(author === userName) {
-    message.classList.add('message--self');
-    message.innerHTML = `<h3 class="message__author">${userName === author ? 'You' : author}</h3>
-    <div class="message__content">${content}</div>`
-    messagesList.appendChild(message);
-  }
-};
+  if(author === userName) message.classList.add('message--self');
+  message.innerHTML = `
+    <h3 class="message__author">${userName === author ? 'You' : author }</h3>
+    <div class="message__content">
+      ${content}
+    </div>
+  `;
+  messagesList.appendChild(message);
+}
--- server.js
+++ server.js
@@ -1,16 +1,37 @@
 const express = require('express');
 const path = require('path');
+const socket = require('socket.io');
 
 const app = express();
 const port = 8000;
 
 const messages = [];
-
+const users = [];
 app.use(express.static(path.join(__dirname, '/client')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/index.html'));
 });
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server is runnig on port ${port}`);
+});
+const io = socket(server);
+io.on('connection', (socket) => {
+  console.log('New client! Its id – ' + socket.id);
+  socket.on('join', (userName) => {
+    console.log('New user ' + socket.id);
+    users.push({ author: userName, id: socket.id });
+    socket.broadcast.emit('message', { author: 'Chat bot', content: `${userName} has joined the converstaion!`});
+  });
+  socket.on('message', (message) => {
+    console.log('Oh, I\'ve got something from ' + socket.id);
+    messages.push(message);
+    socket.broadcast.emit('message', message);
+  });
+  socket.on('disconnect', () => {
+    const user = users.filter(user => (user.id !== socket.id));
+    socket.broadcast.emit('message', { author: 'Chat bot', content: `${user.userName} has left the conversation.`});
+    console.log('isers' + user);
+    console.log('Oh, socket ' + socket.id + ' has left') });
+    console.log('I\'ve added a listener on message event \n');
 });
